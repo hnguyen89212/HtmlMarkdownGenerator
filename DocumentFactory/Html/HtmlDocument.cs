ee1451226615d9c3f9cc73171cf27d139eb539bc
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-18 14:46:51 -0400

    fresh HtmlDocument.cs, no code

---
+++ DocumentFactory/Html/HtmlDocument.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentFactory.Html
+{
+    /// <summary>
+    /// A concrete IDocument implementation to handle Html.
+    /// </summary>
+    public class HtmlDocument : IDocument
+    {
+        public void AddElement(IElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunDocument()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
