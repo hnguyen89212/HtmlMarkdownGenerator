ea895c1d76b79ccc8dc681987051024fc0308653
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-18 13:44:10 -0400

    fresh start

---
+++ Director/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Director
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] commands;
+            var list = File.ReadAllText("CreateDocumentScript.txt");
+            commands = list.Split('#');
+
+            foreach (var command in commands)
+            {
+                var strippedCommand = Regex.Replace(command, @"\t|\n|\r", "");
+                var commandList = strippedCommand.Split(':');
+                switch (commandList[0])
+                {
+                    case "Document":
+                        // Your document creation code goes here
+                        break;
+                    case "Element":
+                        // Your element creation code goes here
+                        break;
+                    case "Run":
+                        // Your document running code goes here
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
