ffb2fee1c656f1ff6199dc91801165fc75fe6c31
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-18 14:46:00 -0400

    fresh MarkdownDocument.cs, no code

---
+++ DocumentFactory/Markdown/MarkdownDocument.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentFactory.Markdown
+{
+    public class MarkdownDocument : IDocument
+    {
+        public void AddElement(IElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunDocument()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
