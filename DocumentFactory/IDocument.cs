c869478a6f3d7a715c221973ea487e2a2b2c221c
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-18 14:23:56 -0400

    created IDocument interface

---
+++ DocumentFactory/IDocument.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentFactory
+{
+    /// <summary>
+    /// An interface dictating the behavior of Document classes.
+    /// </summary>
+    interface IDocument
+    {
+        /// <summary>
+        /// Adds a new element into the current list of elements.
+        /// </summary>
+        /// <param name="element">The element to add</param>
+        void AddElement(IElement element);
+
+        /// <summary>
+        /// Generates a Html & Markdown document in file system
+        /// and opens them in Chrome.
+        /// </summary>
+        void RunDocument();
+    }
+}
